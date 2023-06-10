afb54dfb56d233c735fd45b04d8bc65f134e2a09
ciara1940 <ciaracheah.2021@scis.smu.edu.sg>
2023-06-10 20:03:58 +0800

    wishlist page,nee fix alignment & img dont load

--- tothecloset/src/app/_components/card.tsx
+++ tothecloset/src/app/_components/card.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Heart from "../_images/heart.svg";
 
 type CardProps = {
   name: string;
@@ -6,26 +7,58 @@ type CardProps = {
   price: string;
 };
 
+// const Card: NextPage<CardProps> = ({ name, brand, price }) => {
+//   return (
+//     <div className="w-[200px] h-[400px] text-left text-xs text-dimgray font-lato mb-4">
+//       <div className="top-[50px] my-3 bg-gainsboro w-[180px] h-[270px]"></div>
+//       <b className="leading-[20px] flex text-black items-center">
+//         {brand}
+//       </b>
+//       <div className="leading-[20px] flex items-center">
+//         {name}
+//       </div>
+//       <div className="leading-[20px] flex items-center">
+//         {price}
+//       </div>
+    //   <div className="box-border w-[121px] h-[33px] flex flex-row py-2.5 px-5 items-start justify-start text-center text-2xs text-black border-[1px] border-solid border-black">
+    //       <div className="bg-white w-[83px] h-[13px] z-[0]" ></div>
+    //       <div className="uppercase flex items-center justify-center w-[83px] h-[13px] shrink-0 z-[1]">
+    //         Add to cart
+    //       </div>
+    //     </div>
+    // </div>
+//   );
+// };
 const Card: NextPage<CardProps> = ({ name, brand, price }) => {
   return (
-    <div className="w-[200px] h-[400px] text-left text-xs text-dimgray font-lato mr-4 mb-4">
-    <div className="top-[50px] mr-40 my-3 bg-gainsboro w-[200px] h-[300px]" />
-      <div className="leading-[20px] flex items-center">
-        {name}
-      </div>
-      <b className="leading-[20px] flex text-black items-center">
-        {brand}
-      </b>
-      <div className="leading-[20px] flex items-center">
-        {price}
-      </div>
-      <div className="box-border w-[121px] h-[33px] flex flex-row py-2.5 px-5 items-start justify-start text-center text-2xs text-black border-[1px] border-solid border-black">
-        <div className="bg-white w-[83px] h-[13px] z-[0]" />
-        <div className="uppercase flex items-center justify-center w-[83px] h-[13px] shrink-0 z-[1]">
-          Add to cart
+  
+      <div className="w-[250px] h-[400px] mb-20">
+        <div className="bg-gainsboro h-[100%] w-100">
         </div>
+        <div className="my-2 font-semibold font-lato">
+          {brand}
+        </div>
+        <div className="text-slate-500 font-lato">
+          {name}
+        </div>
+        <div className="text-slate-500 font-lato">
+          {price}
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="my-2 box-border py-3 px-6 border-[1px] w-150px] tracking-[1.5px] flex border-solid border-black">
+            <div className="uppercase flex items-center justify-center">
+              Add to cart
+            </div>
+          </div>
+          <img className="fill-black" src={Heart} alt="heart"></img>
+        </div>
+
+
+          
+        
+
       </div>
-    </div>
+  
   );
 };
 
--- tothecloset/src/app/wishlist/page.tsx
+++ tothecloset/src/app/wishlist/page.tsx
@@ -8,26 +8,70 @@ const WishlistLoggedIn: NextPage = () => {
     { id: 1, name: "Item 1", brand: "Brand 1", price: "69.90 SGD" },
     { id: 2, name: "Item 2", brand: "Brand 2", price: "79.90 SGD" },
     { id: 3, name: "Item 3", brand: "Brand 3", price: "89.90 SGD" },
+    { id: 4, name: "Item 4", brand: "Brand 4", price: "99.90 SGD" },
+    { id: 5, name: "Item 5", brand: "Brand 5", price: "99.90 SGD" },
     // Add more items as needed
   ];
 
   return (
-    <div className="relative bg-white w-full h-[1024px] overflow-hidden text-left text-xs font-lato">
-      <div className="top-[150px] left-[100px] text-[32px] tracking-[5px] uppercase text-gray mt-8 ml-8">
-        Wish List
-      </div>
+  //   <div className="bg-white w-full text-left font-lato">
+  //     <div className="text-[32px] tracking-[5px] uppercase text-gray py-[50px] px-8">
+  //       Wish List
+  //     </div>
 
-      <div className="flex flex-wrap justify-start my-5 mt-4 ml-8">
-        {wishlistItems.map((item) => (
-          <Card
-            key={item.id}
-            name={item.name}
-            brand={item.brand}
-            price={item.price}
-          />
-        ))}
-      </div>
+  //     {/*  grid grid-cols-4 gap-4 mt-4 mx-24 */}
+  //     <div className="flex flex-col items-start">
+  //     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between ">
+  //       {wishlistItems.map((item) => (
+  //         <div className="flex justify-center mx-8 mb-24">
+  //         <Card
+  //           key={item.id}
+  //           name={item.name}
+  //           brand={item.brand}
+  //           price={item.price}
+  //         />
+  //         </div>
+  //       ))}
+  //     </div>
+  //     </div>
+  //   </div>
+  // // );
+  // <div className="bg-white w-full font-lato">
+    
+
+  //     {/*  grid grid-cols-4 gap-4 mt-4 mx-24 */}
+  //     <div className="flex flex-col items-start">
+  //     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between ">
+  //       <div className="mx-8 mb-12 flex justify-center text-[32px] tracking-[2.4px] uppercase py-[50px]">Wish List</div>
+  //       <div className="col-span-1"></div> {/* Empty column */}
+  //       <div className="col-span-1"></div> {/* Empty column */}
+  //       <div className="col-span-1"></div> {/* Empty column */}
+  //       {wishlistItems.map((item) => (
+  //         <div className="flex justify-center mx-8 mb-24">
+    
+  //         <Card
+  //           key={item.id}
+  //           name={item.name}
+  //           brand={item.brand}
+  //           price={item.price}
+  //         />
+  //         </div>
+  //       ))}
+  //     </div>
+  //     </div>
+  //   </div>
+  <div className="bg-white w-full font-lato">
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    <div className="col-span-full text-[32px] tracking-[2.4px] uppercase text-gray py-8">
+      Wish List
     </div>
+    {wishlistItems.map((item) => (
+      <div className="flex justify-center" key={item.id}>
+        <Card name={item.name} brand={item.brand} price={item.price} />
+      </div>
+    ))}
+  </div>
+</div>
   );
 };
 
