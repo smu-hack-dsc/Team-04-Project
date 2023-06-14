13ddfb7513929d5525ca8ed038231fd034b13a05
hannahloh <hannahloh.2021@scis.smu.edu.sg>
2023-06-15 02:57:54 +0800

    Created progress bar for shipping,delivery,payment

--- tothecloset/src/app/order/shipping-details/page.tsx
+++ tothecloset/src/app/order/shipping-details/page.tsx
@@ -1,7 +1,39 @@
+import React from 'react';
+import FirstComponent from '../images/Group 4.png';
+import SecondComponent from '../images/Group 5.png';
 
-export default function Page() {
-    return (
+const Page = () => {
+  return (
     <section className="font=lato">
-    <h1>Delivered</h1>
-    </section>);
-  }
+      <section className="h-20 bg-gray-200"></section> {/* Navigation Bar Section */}
+      <div className="flex items-center justify-center mt-8"> {/* Padding adjustment */}
+        <div className="flex items-center">
+          <div className="w-24 flex-shrink-0 relative flex items-center">
+            <p className="text-center text-2xl text-black mt-1">Shipping</p>
+          </div>
+          <div className="flex items-center ml-15"> {/* Margin and alignment adjustment */}
+            <div className="relative flex items-center">
+              <div className="h-full w-1 bg-gray-300 opacity-30"></div>
+              <div className="w-40 h-8 flex items-center justify-center">
+                <img src={FirstComponent.src} alt="Delivery" className="h-full object-cover absolute" />
+              </div>
+              <div className="h-full w-1 bg-gray-300 opacity-30"></div>
+            </div>
+            <p className="text-center text-2xl text-gray-500 opacity-50 mt-1">Delivery</p>
+          </div>
+          <div className="flex items-center ml-15"> {/* Margin and alignment adjustment */}
+            <div className="relative flex items-center">
+              <div className="h-full w-1 bg-gray-300 opacity-30"></div>
+              <div className="w-40 h-8 flex items-center justify-center">
+                <img src={SecondComponent.src} alt="Payment" className="h-full object-cover absolute" />
+              </div>
+            </div>
+            <p className="text-center text-2xl text-gray-500 opacity-50 mt-1">Payment</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
