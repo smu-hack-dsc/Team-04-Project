dcd334d0512e5ab13d3ee058fae1cb19545bbdff
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-07-17 23:13:29 +0800

    redid login and fixed signup pages

--- src/app/signup/personal-particulars/page.tsx
+++ src/app/signup/personal-particulars/page.tsx
@@ -28,7 +28,10 @@ const PersonalParticularsPage = () => {
     ];
 
     return (
-    <div className='pt-16'>
+    <div className='py-16'>
+      <div className='flex justify-center mb-10 mt-8'>
+        <span className="text-2xl uppercase tracking-[2.4px]">Sign Up</span>
+      </div>
       <div className="flex flex-rows justify-center my-10 mx-6 text-xs sm:text-base">
 
         <div>Personal Particulars</div>
@@ -55,39 +58,39 @@ const PersonalParticularsPage = () => {
           <div className='grid grid-cols-1 md:grid-cols-2 w-full'>
             <div className='p-2'>
               <p>First Name</p>
-              <input type="text" name="firstName" className='outline-none border border-midgrey border-1 w-full h-7 p-2'/>
+              <input type="text" name="firstName" className='outline-none border border-midgrey border-1 w-full h-7 p-2 focus:ring-0 focus:border-grey focus:border-1'/>
             </div>
             <div className='p-2'>
               <p>Last Name</p>
-              <input type="text" name="lastName" className='outline-none border border-midgrey border-1 w-full h-7 p-2'/>
+              <input type="text" name="lastName" className='outline-none border border-midgrey border-1 w-full h-7 p-2 focus:ring-0 focus:border-grey focus:border-1'/>
             </div>
           </div>
 
           <div className='p-2'>
             <p>Email</p>
-            <input type="email" name="email" className='outline-none border border-midgrey border-1 w-full h-7 p-2'/>
+            <input type="email" name="email" className='outline-none border border-midgrey border-1 w-full h-7 p-2 focus:ring-0 focus:border-grey focus:border-1'/>
           </div>
 
           <div className='p-2'>
             <p>Phone Number</p>
             <div className='flex flex-cols'>
-              <select name="countryCode" className='focus:outline-none border border-midgrey border-1 w-16 px-2 h-7 me-2'>
+              <select name="countryCode" className='text-sm focus:outline-none border border-midgrey border-1 w-16 inline-block align-top h-7 me-2 focus:ring-0 focus:border-grey focus:border-1'>
                 {countryCodeArr.map((countryCode, index) =>(
                   <option key={index} value={countryCode} className='text-sm'>{countryCode}</option>
                 ))}
               </select>
-              <input type="number" name="password" className='outline-none border border-midgrey border-1 w-full h-7 p-2 ms-2'/>
+              <input type="text" name="phoneNum" className='outline-none border border-midgrey border-1 w-full h-7 p-2 ms-2 focus:ring-0 focus:border-grey focus:border-1'/>
             </div>
           </div>
 
           <div className='p-2'>
             <p>Password</p>
-            <input type="password" name="password" className='outline-none border border-midgrey border-1 w-full h-7 p-2'/>
+            <input type="password" name="password" className='outline-none border border-midgrey border-1 w-full h-7 p-2 focus:ring-0 focus:border-grey focus:border-1'/>
           </div>
 
           <div className='p-2'>
             <p>Re-enter Password</p>
-            <input type="password" name="reEnteredPassword" className='outline-none border border-midgrey border-1 w-full h-7 p-2'/>
+            <input type="password" name="reEnteredPassword" className='outline-none border border-midgrey border-1 w-full h-7 p-2 focus:ring-0 focus:border-grey focus:border-1'/>
           </div>
         </div>
       </div>
