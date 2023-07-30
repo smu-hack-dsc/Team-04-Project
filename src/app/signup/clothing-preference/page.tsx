47b7ab0713a05edc1311fb1062f122de68e05e80
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-07-30 21:48:23 +0800

    validation for all sign up pages

--- src/app/signup/clothing-preference/page.tsx
+++ src/app/signup/clothing-preference/page.tsx
@@ -20,6 +20,32 @@ const AddressPage = () => {
       setBottomsSliderValue(bottomsValue);
     };
 
+    var [heightValidation, setHeightValidation] = useState("");
+    var [weightValidation, setWeightValidation] = useState("");
+
+    const validation = () => {
+      var emptyFieldError = "ⓘ Field cannot be empty."
+      var errors = 0;
+      // height
+      const heightElement = document.getElementById("height") as HTMLInputElement;
+      if (heightElement.value === "") {
+        setHeightValidation(emptyFieldError);
+        errors += 1
+      } 
+      // weight
+      const weightElement = document.getElementById("weight") as HTMLInputElement;
+      if (weightElement.value === "") {
+        setWeightValidation(emptyFieldError);
+        errors += 1
+      }
+      
+      // if have error
+      if (errors > 0){
+      } else {
+        window.location.href = "/"
+      }
+    }
+
     return (
     <div className='py-16 '>
 
@@ -57,25 +83,27 @@ const AddressPage = () => {
           <div className='p-2'>
             <div className='flex flex-cols items-center'>
               <p className='me-2 inline-block align-middle w-40'>Height</p>
-              <input type="number" name="height" className='outline-none border border-midgrey border-1 w-full h-7 p-2 me-2 focus:ring-0 focus:border-grey focus:border-1'/>
-              <select name="height" className='focus:outline-none border border-midgrey border-1 w-16 px-2 h-7 ms-2 focus:ring-0 focus:border-grey focus:border-1'>
+              <input type="number" name="height" id="height" className='outline-none border border-midgrey border-1 w-full h-7 p-2 me-2 focus:ring-0 focus:border-grey focus:border-1'/>
+              <select name="height" className='focus:outline-none border border-midgrey border-1 w-24 p-0 text-xs px-2 h-7 ms-2 focus:ring-0 focus:border-grey focus:border-1'>
                 {heightArr.map((heightMeasurement, index) =>(
                   <option key={index} value={heightMeasurement} className='text-sm'>{heightMeasurement}</option>
                 ))}
               </select>
             </div>
+            <p className='text-red-500'>{heightValidation}</p>
           </div>
 
           <div className='p-2'>
             <div className='flex flex-cols items-center'>
               <p className='me-2 inline-block align-middle w-40'>Weight</p>
-              <input type="number" name="weight" className='outline-none border border-midgrey border-1 w-full h-7 p-2 me-2 focus:ring-0 focus:border-grey focus:border-1'/>
-              <select name="weight" className='focus:outline-none border border-midgrey border-1 w-16 px-2 h-7 ms-2 focus:ring-0 focus:border-grey focus:border-1'>
+              <input type="number" name="weight" id="weight" className='outline-none border border-midgrey border-1 w-full h-7 p-2 me-2 focus:ring-0 focus:border-grey focus:border-1'/>
+              <select name="weight" className='focus:outline-none border border-midgrey border-1 w-24 p-0 text-xs px-2 h-7 ms-2 focus:ring-0 focus:border-grey focus:border-1'>
                 {weightArr.map((weightMeasurement, index) =>(
                   <option key={index} value={weightMeasurement} className='text-sm'>{weightMeasurement}</option>
                 ))}
               </select>
             </div>
+            <p className='text-red-500'>{weightValidation}</p>
           </div>
 
           <div className='text-center py-5'>
@@ -134,13 +162,17 @@ const AddressPage = () => {
           </button>
         </Link>
 
-        <Link href="/" className='m-4'>
+        <div
+          // href="/" 
+          className='m-4'
+          onClick={validation}
+        >
           <button className="my-2 box-border text-sm py-2 px-6 border-[1px] tracking-[1px] flex border-solid border-black">
             <div className="uppercase flex items-center justify-center">
               Sign Up
             </div>
           </button>
-        </Link>
+        </div>
 
         <Link href="/" className='m-4 flex items-center'>
           <p className='uppercase underline text-xs inline-block align-middle'>
