cb42984ff6f7acd16fefc84550b85bc09c67c9eb
ciara1940 <ciaracheah.2021@scis.smu.edu.sg>
2023-08-01 23:58:01 +0800

    fixed module import

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import BrowsingCard from "../_components/BrowsingCard";
 import { DatePicker } from 'antd';
 import moment, { Moment } from 'moment';
