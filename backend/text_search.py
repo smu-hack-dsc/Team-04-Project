1733e3ee0e3f748fa1871b618e3b56e0dd7ff70b
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-08-06 00:45:16 +0800

    More prompt engineering to reduce bad outputs

--- backend/text_search.py
+++ backend/text_search.py
@@ -7,7 +7,7 @@ def getCategories(apiKey, input):
     # prompt = "Given the prompt \"" + input + "\" please give me the categories that are relevant to it from this list. " \
     #     "[Black, White, Red, Blue, Orange, Yellow, Green, Blue, Indigo, Top, Bottom, Full Body, Shirt, T-Shirt, Pants, Long Pant, Long Sleeved Shirt, Dress]" \
     #     "Please respond only with the categories without any other words"
-    prompt = "Given the prompt \"" + input + "\" please give me the color, clothing type and brands that are relevant to it. Only give brands that exist in the prompt. There can be more than one of each category. Please respond only with the categories without any other words" 
+    prompt = "Given the prompt \"" + input + "\" please give me the color, clothing type and brands that are in the prompt. Only give the categories if the word is fully in the prompt. There can be more than one of each category. Please respond only with the categories without any other words" 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature = 0.2,
@@ -21,7 +21,8 @@ def getCategories(apiKey, input):
     finalArr = []
     regex_pattern = r".*?: "
     for categories in unprocessedArr:
-        finalArr.append(re.sub(regex_pattern, "", categories).split(", "))
+        if categories != "":
+            finalArr.append(re.sub(regex_pattern, "", categories).split(", "))
     # color = []
     # type = []
     # for categories in unprocessedArr:
