45a46d9e0c542cf8de9ac3c00626b11f9a7fbd7e
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-08-06 00:10:21 +0800

    Changed the prompt for openai call making it more flexible and include brand

--- backend/text_search.py
+++ backend/text_search.py
@@ -1,11 +1,13 @@
 import openai
+import re
 
 
 def getCategories(apiKey, input):
     openai.api_key = apiKey
-    prompt = "Given the prompt \"" + input + "\" please give me the categories that are relevant to it from this list. " \
-        "[Black, White, Red, Blue, Orange, Yellow, Green, Blue, Indigo, Top, Bottom, Full Body, Shirt, T-Shirt, Pants, Long Pant, Long Sleeved Shirt, Dress]" \
-        "Please respond only with the categories without any other words"
+    # prompt = "Given the prompt \"" + input + "\" please give me the categories that are relevant to it from this list. " \
+    #     "[Black, White, Red, Blue, Orange, Yellow, Green, Blue, Indigo, Top, Bottom, Full Body, Shirt, T-Shirt, Pants, Long Pant, Long Sleeved Shirt, Dress]" \
+    #     "Please respond only with the categories without any other words"
+    prompt = "Given the prompt \"" + input + "\" please give me the color, clothing type and brands that are relevant to it. Only give brands that exist in the prompt. There can be more than one of each category. Please respond only with the categories without any other words" 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature = 0.2,
@@ -15,12 +17,16 @@ def getCategories(apiKey, input):
         ]
     )
 
-    unprocessedArr = completion.choices[0].message.content.split(", ")
-    color = []
-    type = []
+    unprocessedArr = completion.choices[0].message.content.split("\n")
+    finalArr = []
+    regex_pattern = r".*?: "
     for categories in unprocessedArr:
-        if categories in ["Black", "White", "Red", "Blue", "Orange", "Yellow", "Green", "Blue", "Indigo"]:
-            color.append(categories)
-        elif categories in ["Top", "Bottom", "Full Body", "Shirt", "T-Shirt", "Pants", "Long Pant", "Long Sleeved Shirt", "Dress"]:
-            type.append(categories)
-    return [color, type]
+        finalArr.append(re.sub(regex_pattern, "", categories).split(", "))
+    # color = []
+    # type = []
+    # for categories in unprocessedArr:
+    #     if categories in ["Black", "White", "Red", "Blue", "Orange", "Yellow", "Green", "Blue", "Indigo"]:
+    #         color.append(categories)
+    #     elif categories in ["Top", "Bottom", "Full Body", "Shirt", "T-Shirt", "Pants", "Long Pant", "Long Sleeved Shirt", "Dress"]:
+    #         type.append(categories)
+    return finalArr
