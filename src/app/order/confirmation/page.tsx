9bf60900d4b2a4fc25a5160c914511781d23cddc
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-08-08 23:01:48 +0800

    payment confirmation link

--- src/app/order/confirmation/page.tsx
+++ src/app/order/confirmation/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link"
 
 const Page = () => {
   return (
@@ -26,9 +27,11 @@ const Page = () => {
           className="my-2 mt-20 box-border text-sm py-2 px-6 border-[1px] tracking-[1px] flex border-solid border-black
           bg-white text-black hover:bg-black hover:text-white"
         >
-          <div className="uppercase flex items-center justify-center">
-            Back to shopping
-          </div>
+          <Link href="/">
+            <div className="uppercase flex items-center justify-center">
+              Back to shopping
+            </div>
+          </Link>
         </button>
       </div>
     </section>
