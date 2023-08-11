c1b7e295514a93a76f7fa70bb21c96b6ea802f03
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-08-12 06:05:04 +0800

    fix vercel issue pt 2

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -21,56 +21,14 @@ interface Product {
 const WishlistLoggedIn: NextPage = () => {
   const [wishlistItems, setWishlistItems] = useState<WishlistItem[]>([]);
   const [products, setProducts] = useState<Product[]>([]);
-  const SECRET_KEY =
-    "q9grv7k_5P07NZ7pz2k2r3wonSbNF2tJgTNf5zVaj9mHvrD_3H4aKGeOZq0yKpgv";
-  const [userToken, setUserToken] = useState("");
+
   const [userId, setUserId] = useState(""); // Add this state to store user ID
 
-  useEffect(() => {
-    // Check if the user token exists in session storage
-    const token =
-      typeof window !== "undefined" ? sessionStorage.getItem("token") : null;
-
-    // Update the state with the user token
-    setUserToken(token);
-    console.log("user token:", userToken); // This might still show the old value
-
-    // Decode the user token to get the user ID
-    const decodedToken = decodeToken(token);
-    if (decodedToken) {
-      setUserId(decodedToken.user_id); // Store the decoded user ID in state
-    }
-  }, [setUserId]);
-
-  const decodeToken = (token) => {
-    try {
-      if (!token) {
-        console.error("Token is undefined or null.");
-        return null;
-      }
-
-      // Access the secret key from the environment variables or hardcoded value
-      const secretKey =
-        SECRET_KEY ||
-        "q9grv7k_5P07NZ7pz2k2r3wonSbNF2tJgTNf5zVaj9mHvrD_3H4aKGeOZq0yKpgv";
-      console.log("secret_key", secretKey);
-
-      // Decode the token using the provided secret key
-      const decodedToken = jwt.verify(token, secretKey);
-      console.log("userinfo:", decodedToken.user_id);
-      setUserId(decodedToken.user_id);
-      sessionStorage.setItem("userId", decodedToken.user_id); // Store in session storage
-      return decodedToken;
-    } catch (error) {
-      console.error("Error decoding token:", error);
-      return null;
-    }
-  };
-  const user_id = sessionStorage.getItem("userId"); // Replace with the actual user ID or retrieve it from your authentication system
 
   useEffect(() => {
+    const userId = sessionStorage.getItem("userId");
     axios
-      .get(`http://localhost:5000/api/wishlist/${user_id}/`)
+      .get(`http://localhost:5000/api/wishlist/${userId}/`)
       .then((response) => {
         console.log("response:", response.data);
         setWishlistItems(response.data); // Update the wishlist items state with the data from the API
@@ -78,7 +36,7 @@ const WishlistLoggedIn: NextPage = () => {
       .catch((error) => {
         console.error("Error fetching wishlist:", error);
       });
-  }, [user_id]);
+  }, [userId]);
 
   useEffect(() => {
     const fetchProductDetails = async () => {
